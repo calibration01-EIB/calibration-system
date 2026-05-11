2a9a8d326e4d6a1e9eb1c505b3adf0a73dd4698d
calibration01-EIB <testercalibration01@gmail.com>
2026-05-11 15:42:29 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'cal-v1';
+const ASSETS = [
+  '/calibration-system/',
+  '/calibration-system/index.html',
+  '/calibration-system/icon-192.png',
+  '/calibration-system/icon-512.png',
+  '/calibration-system/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('supabase.co')) return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
